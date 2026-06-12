8297137d1d6b41ee6f66662ed6342aabcffdb4b3
Molly Arora <aroramolly54@gmail.com>
2026-06-12 15:30:42 +0530

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	int x ;
+	cin>>x;
+	
+	if (x%5 !=0 ){
+	    cout<< "yes";
+	}
+	else {
+	    cout<<"no";
+	}
+	return 0;
+
+}
